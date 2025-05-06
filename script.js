835004416fe5c80e809d8f4e2c52808c442ad41b
Enomi-4mg <miyashu2741@gmail.com>
2025-05-06 19:25:36 +0900

    メニューボタンの実装・Diaryの更新

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 'use strict';
+const toggleButton = document.querySelector('.menu_toggle');
+const sidebar = document.querySelector('.sidebar');
+
+toggleButton.addEventListener('click', () => {
+  sidebar.classList.toggle('show');
+});
